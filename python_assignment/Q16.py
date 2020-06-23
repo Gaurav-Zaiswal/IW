a4e072aa00e31694334b7f745c9ca90eab3abbeb
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-06-23 19:50:14 +0545

    Question_16 is completed

---
+++ python_assignment/Q16.py
@@ -0,0 +1,16 @@
+# Write a Python program to sum all the items in a list.
+
+def sum_of_items(arg):
+    '''
+
+    :param arg: list of numbers
+    :return: sum:number
+    '''
+    sum = 0
+    for _ in arg:
+        sum += _
+    return sum
+
+
+if __name__ == '__main__':
+    print(sum_of_items([2, 4, 5, 6,7 ,8]))
