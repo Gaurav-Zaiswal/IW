8b5296253a17f523edef90f05c18cfb7aed1e6af
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-06-26 09:56:14 +0545

    Question_24 is completed

---
+++ python_assignment/Q24.py
@@ -0,0 +1,14 @@
+# Write a Python program to clone or copy a list.
+
+def clone_List(arg):
+    '''
+
+    :param arg: list of numbers
+    :return: Boolean
+    '''
+    new_arg = arg
+    return new_arg
+
+
+if __name__ == '__main__':
+    print(clone_List([2, 1, 8 ,4, 8, 3, 2, 1, 5, 4]))
