e961219214f2cda31ae92ac4aed8a3a8a6187bef
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-06-27 11:52:02 +0545

    Question_function_4 was completed

---
+++ python_assignment/Qf4.py
@@ -0,0 +1,13 @@
+# Write a Python program to reverse a string.
+
+def reverse_str(arg):
+    '''
+
+    :param arg: string
+    :return: string
+    '''
+
+    return arg[::-1]
+
+if __name__ == "__main__":
+    print(reverse_str('apple'))
