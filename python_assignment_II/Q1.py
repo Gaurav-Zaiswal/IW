e0a72d07ed0abd42302699dc0273efb52b183e83
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-06-29 23:16:55 +0545

    new dir for [Python Assignment II] was created and Q1 was solved

---
+++ python_assignment_II/Q1.py
@@ -0,0 +1,9 @@
+# Create a variable, paragraph, that has the following content:
+# "Python is a great language!", said Fred. "I don't ever remember
+# having this much fun before."
+
+if __name__ == "__main__":
+    var = """ "Python is a great language!", said Fred. "I don't ever remember
+             having this much fun before." """
+
+    print(var)
