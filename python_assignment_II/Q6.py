62bb1c4a643808ecd39ff47a12ad3381d501d979
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-07-02 07:48:01 +0545

    Question_6 was completed

---
+++ python_assignment_II/Q6.py
@@ -0,0 +1,9 @@
+# Create a list with the names of friends and colleagues. Search for the
+# name ‘John’ using a for a loop. Print ‘not found’ if you didn't find it.
+
+friend_list = ['Ram', 'Jesus', 'Allah', 'Budha']
+
+print('List of names:\n', friend_list)
+print('Searching for John...')
+if not 'John' in friend_list:
+    print('John not found!')
