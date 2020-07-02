64509981930540bb9dcd02caeb32e97d799c0750
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-07-02 18:02:42 +0545

    Question_10 was completed

--- python_assignment_II/Q10.py
+++ python_assignment_II/Q10.py
@@ -6,4 +6,17 @@
 
 def change_case(camel_case=None, kebab_case=None):
     if camel_case:
+        snake_case = ''
+        snake_case += camel_case[0].lower()
+        for _ in camel_case[1:]:
+            if ord(_) in range(65, 91):
+                snake_case += '_'
+            snake_case += _
+        return snake_case.lower()
 
+    if kebab_case:
+        return kebab_case.replace("-", "_")
+
+
+print('ThisIsNepal: ', change_case(camel_case="ThisIsNepal"))
+print('this-is-nepal: ', change_case(kebab_case="this-is-nepal"))
