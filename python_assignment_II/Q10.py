ac97c2dd2ac90e56712899b01a63382f090b4e6c
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-07-02 16:02:41 +0545

    Question_9 was completed

---
+++ python_assignment_II/Q10.py
@@ -0,0 +1,9 @@
+# Write a function that takes camel-cased strings (i.e.
+# ThisIsCamelCased), and converts them to snake case (i.e.
+# this_is_camel_cased). Modify the function by adding an argument,
+# separator, so it will also convert to the kebab case
+# (i.e.this-is-camel-case) as well.
+
+def change_case(camel_case=None, kebab_case=None):
+    if camel_case:
+
--- python_assignment_II/Q9.py
+++ python_assignment_II/Q9.py
@@ -2,37 +2,38 @@
 # the item it is looking for. It should return the index of the item if found.
 # It should return -1 if the item is not found.
 
-def binary_search(arr, key):
+def binary_search(arr, key, index=0):
     '''
 
     :param arr: list
     :param key: number
     :return: number
     '''
-    print(arr, key)
+    # print(arr, key)
     length = len(arr)
-    if 0 < length-1:
+    if 0 <= length-1:
         mid =  length // 2
         # print('mid index: ',mid)
         mid_item = arr[mid]
-        print('mid item: ',mid_item)
+        # print('mid item: ',mid_item)
+        index += mid
 
         if mid_item == key:
-            return mid
+            return index
 
         elif key < mid_item:
             left_arr = arr[:mid]
             # print('left child: ',left_arr)
-            binary_search(left_arr, key)
+            return binary_search(left_arr, key, index - mid)
 
-        elif key > mid_item:
+        else:
             right_arr = arr[mid+1:]
             # print('right child: ', right_arr)
-            binary_search(right_arr, key)
+            return binary_search(right_arr, key, index + 1)
 
+    return -1
 
-    return '-1'
 
 arr = [1,2,3,4,5,6,7,8,9]
-key = 7
+key = 6
 print(binary_search(arr, key))
