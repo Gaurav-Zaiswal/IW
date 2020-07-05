8d68ceafd064c40e2bd86e45d839239b970252f5
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-07-05 22:44:38 +0545

    Question_13 was completed

--- python_assignment_II/Q13.py
+++ python_assignment_II/Q13.py
@@ -3,3 +3,28 @@
 # tuples should have a name, address, and age. The file should create
 # a header row followed by a row for each tuple.
 
+import csv
+
+def export_to_csv(file_name, args) -> None:
+    '''
+
+    :param file_name: String -> file name without extension
+    :param args: list -> list of tuples, tuples has name, address, and age in it consecutively
+    :return: None
+    '''
+    result_file_name = file_name + '.csv'
+    with open(result_file_name, mode='w') as employee_file:
+        fieldnames = ['Name', 'Address', 'Age']
+        employee_writer = csv.DictWriter(employee_file, fieldnames=fieldnames)
+
+        employee_writer.writeheader()
+        for arg in args:
+            employee_writer.writerow({'Name': arg[0],
+                                      'Address': arg[1],
+                                      'Age': arg[2]
+                                      })
+
+# calling the function
+export_to_csv('my_friends_info', [('abc', 'asdfd', 23),
+                                  ('mohan', 'ramechap', 90),
+                                  ('mahadev', 'himalayas', 1)])
