caf0ac31dff52dc678faf7538c6c155ef471e140
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-07-05 19:33:55 +0545

    Question_20 was completed

--- python_assignment_II/Q20.py
+++ python_assignment_II/Q20.py
@@ -3,14 +3,15 @@
 
 def find_sublists(arr):
     list_of_sub_lists = []
-    for i in range(len(arr)-1):
+    for i in range(len(arr)):
         sub_list = []
-        sum_of_two_elements = arr[i] + arr[i+1]
-        if -sum_of_two_elements in arr:
-            sub_list.append(arr[i])
-            sub_list.append(arr[i+1])
-            sub_list.append(-sum_of_two_elements)
-            list_of_sub_lists.append(sub_list)
+        for j in range(i+1, len(arr)-i):
+            sum_of_two_elements = arr[i] + arr[j]
+            if -sum_of_two_elements in arr and -sum_of_two_elements not in sub_list:
+                sub_list.append(arr[i])
+                sub_list.append(arr[j])
+                sub_list.append(-sum_of_two_elements)
+                list_of_sub_lists.append(sub_list)
 
     return list_of_sub_lists
 
